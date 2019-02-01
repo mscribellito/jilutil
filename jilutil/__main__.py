1f7c6201bde2a7483c8bf1c093e8e9a6585c3505
Michael Scribellito <mscribellito@users.noreply.github.com>
2019-02-01 11:04:02 -0500

    Add files via upload

--- jilutil/__main__.py
+++ jilutil/__main__.py
@@ -13,15 +13,15 @@ if __name__ == '__main__':
     parser.add_argument('path', type=str, help='path to JIL source file')
 
     actions = parser.add_mutually_exclusive_group(required=True)
-    actions.add_argument('-e', '--export',   action='store_true',    help='Exports jobs contained in the JIL source file in ascending order by name to a CSV file.')
-    actions.add_argument('-f', '--format',   action='store_true',    help='Formats jobs contained in the JIL source file in ascending order by name.')
-    actions.add_argument('-o', '--output',   action='store_true',    help='Outputs jobs contained in the JIL source file in ascending order by name to standard out.')
+    actions.add_argument('-e', '--export',  action='store_true',    help='Exports jobs contained in the JIL source file in ascending order by name to a CSV file.')
+    actions.add_argument('-f', '--format',  action='store_true',    help='Formats jobs contained in the JIL source file in ascending order by name.')
+    actions.add_argument('-o', '--output',  action='store_true',    help='Outputs jobs contained in the JIL source file in ascending order by name to standard out.')
 
     parser.add_argument('-n', '--new',      action='store_true',    help='Formats as new file.')
     parser.add_argument('-r', '--reverse',  action='store_true',    help='Sorts jobs in descending order by name.')
 
     parser.add_argument('-v', '--verbose',  action='store_true',    help='Increases output verbosity.')
 
-    parser.add_argument('--version', action='version', version=__version__)
+    parser.add_argument('--version',        action='version',       version=__version__)
     
     exit(main(parser.parse_args()))
