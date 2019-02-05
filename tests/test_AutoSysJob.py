8935db189dca8a451e9e4df7a746094c54618859
Michael Scribellito <mscribellito@users.noreply.github.com>
2019-02-05 13:07:13 -0500

    Add AutoSysJob tests

--- tests/test_AutoSysJob.py
+++ tests/test_AutoSysJob.py
@@ -20,7 +20,24 @@ class TestAutoSysJob(unittest.TestCase):
     maxDiff = None
 
     def test_from_str(self):
+
         job = AutoSysJob.from_str(jil_job)
+        
+        self.assertIsInstance(job, AutoSysJob)
+    
+    def test_to_str(self):
+
+        job = AutoSysJob('SAMPLE_BOX_JOB')
+        job.attributes['job_type'] = 'BOX'
+        job.attributes['alarm_if_fail'] = '1'
+        job.attributes['date_conditions'] = '1'
+        job.attributes['days_of_week'] = 'su,mo,tu,we,th,fr,sa'
+        job.attributes['description'] = '"Sample box job"'
+        job.attributes['group'] = 'SOME_GROUP'
+        job.attributes['owner'] = 'root@domain'
+        job.attributes['permission'] = 'gx,ge,wx,we,mx,me'
+        job.attributes['start_times'] = '"20:00"'
+
         self.assertEqual(str(job), jil_job)
 
 if __name__ == '__main__':
